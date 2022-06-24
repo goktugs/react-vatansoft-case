8e248ceb795376e70ff4edad26deff80fd73e4f7
Goktug Sevgil <86847572+GoktugS@users.noreply.github.com>
2022-06-24 14:14:11 +0300

    Add axios-db connection

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-export default function login() {
+export default function Login() {
+  const [users, setUsers] = useState();
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetch('http://localhost:3004/accounts')
+        .then((res) => res.json())
+        .then((data) => setUsers(data));
+    };
+    fetchData();
+  }, []);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="bg-[#EA5959] w-full h-screen flex items-center justify-center flex-col font-poppins font-bold">
       <h1 className="text-white text-4xl mb-12 text-center">
@@ -14,6 +32,7 @@ export default function login() {
             className="w-40 rounded-2xl placeholder:italic placeholder:text-slate-400 placeholder:text-sm placeholder:pl-3 pl-3 bg-transparent border-4 "
             placeholder="Username"
             type="text"
+            onChange={(e) => setUsername(e.target.value)}
           />
         </div>
         <div className="mb-20 flex w-96 justify-between">
@@ -22,17 +41,20 @@ export default function login() {
             className="w-40 rounded-2xl placeholder:italic placeholder:text-slate-400 placeholder:text-sm placeholder:pl-3 pl-3 bg-transparent border-4 "
             type="password"
             placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <div className="w-60 ">
           <Link
             to="/todo"
             className="bg-gradient-to-r from-gray-200 via-gray-400 to-gray-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 font-poppins w-full inline-block font-regular "
+            onClick={handleLogin}
           >
             Login
           </Link>
         </div>
       </form>
+      {users != null ? console.log(users[0]) : null}
     </div>
   );
 }
